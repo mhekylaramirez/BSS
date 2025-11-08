293534c7f3837f1f10ff61f8eb0868501ee2f6a9
mhekylaramirez <23-02917@g.batstate-u.edu.ph>
2025-11-08 23:49:40 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,4 +32,15 @@ function App() {
   )
 }
 
-export default App
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My Notebook Converter!</h1>
+      <p>This is your React app running locally.</p>
+    </div>
+  );
+}
+
+export default App;
